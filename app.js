095168b8e8b87f55f1cc8348dd45834f9fb3fed4
Artūras Sotničenko <a.sotnicenko@gmail.com>
2017-09-16 21:32:47 +0300

    added few node packages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 var altcoin = require('node-altcoin')()
 
 altcoin.auth(process.env.rpcuser, process.env.rpcpassword)
-altcoin.set('host', process.env.rpcconnect)
+altcoin.set('host', 127.0.0.1)
 altcoin.set({port:process.env.rpcport})
 altcoin.getDifficulty(function() {
     console.log(arguments);
