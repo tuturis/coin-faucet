fc11a1e336438986dd53479828fb9a9eef3f8c2a
Artūras Sotničenko <a.sotnicenko@gmail.com>
2017-09-16 22:13:06 +0300

    chekpoint

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
+
 require('dotenv').config()
-var altcoin = require('node-altcoin')()
+const express = require('express');
+const session = require('express-session');
+const recaptcha = require('express-recaptcha');
+const compression = require('compression');
+
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const altcoin = require('node-altcoin')()
+
+const _ = require('underscore');
+const path = require('path');
+const chalk = require('chalk');
+
+const app = express();
+
+recaptcha.init(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(compression());
+/**
+ * Controllers (route handlers).
+ */
+const faucetController = require('./controllers/faucet');
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
+app.get('/', faucetController.index);
 
 altcoin.auth(process.env.rpcuser, process.env.rpcpassword)
-altcoin.set('host', 127.0.0.1)
+altcoin.set('host', process.env.rpchost)
 altcoin.set({port:process.env.rpcport})
 altcoin.getDifficulty(function() {
     console.log(arguments);
 })
+
+app.listen(app.get('port'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+});
+
+module.exports = app;
