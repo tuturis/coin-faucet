56b10f8f65872cfee291552280ed4ccf909a6b22
Artūras Sotničenko <a.sotnicenko@gmail.com>
2017-09-17 03:13:26 +0300

    removed timestaps from proxy filter model

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ Faucet written in express mongo
 
 # TODO LIST
 1. implement databse (mysql / mongodb?)
-2. add proxy filter
-3. add ssl?
+2. add ssl?
 4. build frontend
 5. add ads
 
--- models/proxy_list.js
+++ models/proxy_list.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const pfSchema = new mongoose.Schema({
   ip:  { type: String, unique: true },
-  }, { timestamps: true });
+  }, { timestamps: false });
 
 const Pf = mongoose.model('Pf', pfSchema);
 module.exports = Pf;
