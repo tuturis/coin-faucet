7a98dc60d6f1c372368bde67a3701392dcee3f76
Artūras Sotničenko <a.sotnicenko@gmail.com>
2017-09-21 19:58:09 +0300

    stated working on cron job

--- cronjob.js
+++ cronjob.js
@@ -5,6 +5,7 @@ const CronJob = require('cron').CronJob;
 const ProxyLists = require('proxy-lists');
 const request = require('request');
 const Pf = require('./models/proxy_list');
+const PQ = require('./models/paymentQ');
 const _ = require('underscore');
 
 mongoose.Promise = global.Promise;
@@ -13,7 +14,7 @@ mongoose.connection.on('error', (err) => {
   console.error(err);
   process.exit();
 });
-
+/*
 var job = new CronJob({
   cronTime: '0 * * * *',
   onTick: function() {
@@ -23,9 +24,32 @@ var job = new CronJob({
   timeZone: 'America/Los_Angeles'
 });
 job.start();
-
+var job2 = new CronJob({
+  cronTime: '15,45 * * * *',
+  onTick: function() {
+    payToPq()
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+job2.start();
+*/
+function payToPq() {
+  var bulk = PQ.collection.initializeOrderedBulkOp();
+      bulk.find({'claimed': false}, (err, results) => {
+        if(err) {
+          console.log(err)
+          process.exit()
+        }
+        console.log(results)
+      }).update({$set: {'claimed': false}});
+      bulk.execute(function (error) {
+           callback();                   
+      });
+}
+payToPq()
 function getPf() {
-  request(' https://www.dan.me.uk/torlist/', function (error, response, body) {
+  request('https://www.dan.me.uk/torlist/', function (error, response, body) {
     body.toString().split('\r\n').forEach((line) => {
       console.log(line)
       pf = new Pf()
