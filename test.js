a57fcd4682f37b08602516c379247bc8ea30ff2c
Artūras Sotničenko <a.sotnicenko@gmail.com>
2017-09-27 01:24:29 +0300

    added test

---
+++ test.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config()
+const mongoose = require('mongoose');
+/*const altcoin = require('node-altcoin')({
+      passphrasecallback: function(command, args, callback) {
+        console.log(` pass = ${process.env.WALLET_PASSPHRASE}`)
+        callback(null, process.env.WALLET_PASSPHRASE, 120);
+    }
+});
+*/
+const config = require('./config')
+
+const Client = require('bitcoin-core');
+const client = new Client({
+  username: process.env.rpcuser,
+  password: process.env.rpcpassword,
+  port: process.env.rpcport,
+  host: process.env.rpchost
+});
+
+const CronJob = require('cron').CronJob;
+const ProxyLists = require('proxy-lists');
+const request = require('request');
+const Pf = require('./models/proxy_list');
+const PQ = require('./models/paymentQ');
+const _ = require('underscore');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  process.exit();
+});
+
+PQ.find({'claimed': false}, (err, results) => {
+  if(err) {
+    console.log(err)
+    /*process.exit()*/
+  }
+  console.log()
+}) 
+.setOptions({ multi: true })
+.update({$set: {'claimed': true}}, (err, success) => {
+  if(err) {console.log(err)};
+  console.log(`up ${success}`)
+}); 
