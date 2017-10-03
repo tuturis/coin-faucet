a0b12870309cd341edc7f99cecb67b5e84f8131e
root <root@vps-71f5b45e9b08c344.4xlhut80856.domain.tld>
2017-10-03 03:03:48 +0000

    config save

--- config.js
+++ config.js
@@ -2,23 +2,28 @@ var config = {
 	payout: {
 		min : 0.0001,
 		max : 0.001,
-		interval: 2  //in hours
+		interval: 2,
 		treshold: 0.01
 	},
 	coin: {
-		name: "Lite Coins",
+		name: "LiteCoin",
 		address: "LgQxUzvGLu6Rj4VmH8kXTmhz3BVEoNKi8g",
-		info : ""
+		info : "Litecoin is a peer-to-peer Internet currency that enables instant, near-zero cost payments to anyone in the world. Litecoin is an open source, global payment network that is fully decentralized without any central authorities. Mathematics secures the network and empowers individuals to control their own finances. Litecoin features faster transaction confirmation times and improved storage efficiency than the leading math-based currency. With substantial industry support, trade volume and liquidity, Litecoin is a proven medium of commerce complementary to Bitcoin"
 	},
 	site: {
 		name: "LiteFaucet.win"
 	},
 	ads: {
-		aads: 676012,
-		coinurl: 66674
+		aads: "676012",
+		coinurl: "66674"
 	},
 	analytics:{
 		google: "UA-106566819-2"
-	}
+	},
+	exchanges: [{
+		name: 'novaexchange.com',
+		url: 'https://novaexchange.com/market/BTC_LTC/?re=pud127q1c1b3989waadm',
+		banner: ''
+	}]
 }
 module.exports = config
