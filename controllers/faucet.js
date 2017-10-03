0aaa4264876c833757b6bb1080a924f3bca10bcb
Artūras Sotničenko <a.sotnicenko@gmail.com>
2017-10-03 12:22:29 +0300

    up

--- controllers/faucet.js
+++ controllers/faucet.js
@@ -204,7 +204,7 @@ exports.checkReferrals = (req, res, next) => {
             }
         })
     } else {
-        Ref.findOne({'address' : req.body.address}, (err, ref) => {
+        Ref.find({'address' : req.body.address}, (err, ref) => {
             if(err) {
                 console.log(`ERR ${JSON.stringify(err)}`);
             }
