a8dd0d8672332cdc1e8e14147b1491afd5a40108
Arturas Sotnicenko <tr3nktass@gmail.com>
2017-11-01 05:08:37 +0200

    up

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const chalk = require('chalk');
 
 const app = express();
 
-captcha.init(process.env.COINHIVE_SITE_KEY, process.env.COINHIVE_SECRET_KEY,{whitelabel:false, hashes: 10240});
+captcha.init(process.env.COINHIVE_SITE_KEY, process.env.COINHIVE_SECRET_KEY,{whitelabel:false, hashes: 9984});
 /**
  * Connect to MongoDB.
  */
