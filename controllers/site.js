090ab781a568554ce66804670d525b5b8abd3c16
Arturas Sotnicenko <tr3nktass@gmail.com>
2017-11-03 22:21:34 +0200

    updated init config

--- controllers/site.js
+++ controllers/site.js
@@ -13,49 +13,62 @@ const config = require('../config')
 // altcoin.set({port:process.env.rpcport})
 
 exports.init = (cb) => {
-    sConfig.count({} , (err, count) => {
-        if (count > 0) {
+    sConfig.count({}, (err, count) => {
+        if (count > 1) {
             cb('config already exits.. skiping')
         } else {
             iConfig = new sConfig()
             iConfig.siteConfig = {
                 payout: {
-                    min: 0.0001,
-                    max: 0.0015,
-                    interval: 2,
-                    treshold: 0.003,
-                    referralCommision: 0.05,
+                    min: 0.00001,
+                    max: 0.0001,
+                    interval: 1,
+                    treshold: 0.004,
+                    referralCommision: 0.07,
+                    profit: 0.5                    
                 },
                 coin: {
-                    ticker: "ONION",
-                    name: "DeepOnion",
-                    address: "DmkLHSA2nPrznFRhoyQ2hZEVwstK4kxtNz",
-                    info: "Deep Onion coins are Freely airdropped/distributed! NO ICO/Crowdfunding! Deep Onion is a hybrid cryptocurrency that uses proof of stake (PoS) and the X13 proof of work (PoW) algorithm. It is natively integrated with the TOR network and ALL connections are made over the TOR network. DeepOnion increase the level of privacy for crypto users to reduce the likelihood of being hacked or attacked by other legal & illegal entities. With DeepOnion, you can send and receive onions (ONION) over the TOR network. Click here to learn more... Security will be further enhanced by DeepSend technologies, to be announced and implemented later. our Anonymity is guaranteed! DeepOnion dev team is composed of industrial, social media, SEM experts and top level block-chain developers. More advanced features will be added to the coin, and the dev team is committed to long term support. The best part is that we DO NOT do ICOs or crowdfunding. 90% of total coins are premined at genesis block. The remaining 10% will be mineable by the public. Most premined coins will be freely airdropped to the community through many rounds of airdrops (detailed herein). Certain conditional requirements apply, please read on for details. Genesis block, premined coins, breakdown; 70% will be air-dropped to the community, while 20% will be used for bounties, rewards and other promotions. Remaining 10% will belong to the dev team. Deep Onion Website: https:\/\/deeponion.org"
+                    ticker: 'LTC',
+                    decimals: 7,
+                    name: "Litecoin",
+                    address: "Lh3eALGy4TESMDayoumncyDFGtH2f6bm44",
+                    info: "Litecoin is a peer-to-peer Internet currency that enables instant, near-zero cost payments to anyone in the world. Litecoin is an open source, global payment network that is fully decentralized without any central authorities. Mathematics secures the network and empowers individuals to control their own finances. Litecoin features faster transaction confirmation times and improved storage efficiency than the leading math-based currency. With substantial industry support, trade volume and liquidity, Litecoin is a proven medium of commerce complementary to Bitcoin"
                 },
                 site: {
-                    name: "onionfaucet.win",
-                    explorer: "http://explorer.deeponion.org//tx"
+                    name: "litefaucet.win",
+                    explorer: "https://live.blockcypher.com/ltc/tx/",
+                    captchaHashes: 5120
                 },
                 ads: {
-                    coinurl: "66663",
-                    aads: "675642",
+                    aads: "676012",
+                    coinurl: "66674",
                     top: "",
-                    top2: [{ "type": "Element", "tagName": "iframe", "attributes": { "dataset": { "aa": 675642 }, "src": "//ad.a-ads.com/675642?size=728x90", "scrolling": "no", "style": { "margin": "0 auto", "width": "728px", "height": "90px", "border": "0px", "padding": 0, "overflow": "hidden" }, "allowtransparency": "true" }, "children": [] }],
-                    left: [{ "type": "Element", "tagName": "a", "attributes": { "href": "https://cex.io/r/0/up105394792/0/", "target": "_blank" }, "children": [{ "type": "Element", "tagName": "img", "attributes": { "src": "https://cex.io/rb/CEX-3-160x600.png", "alt": "CEX.IO Bitcoin Exchange", "width": 160, "height": 600, "border": 0 }, "children": [] }] }],
-                    right: [{ "type": "Element", "tagName": "iframe", "attributes": { "dataset": { "aa": 680336 }, "src": "//ad.a-ads.com/680336?size=120x600", "scrolling": "no", "style": { "width": "120px", "height": "600px", "border": "0px", "padding": 0, "overflow": "hidden" }, "allowtransparency": "true" }, "children": [] }],
+                    top2: [{ "type": "Element", "tagName": "iframe", "attributes": { "dataset": { "aa": 676012 }, "src": "//ad.a-ads.com/676012?size=728x90", "scrolling": "no", "style": { "margin": "0 auto", "width": "728px", "height": "90px", "border": "0px", "pad\nding": 0, "overflow": "hidden" }, "allowtransparency": "true" }, "children": [] }],
+                    right: '',
+                    left: [{ "type": "Element", "tagName": "a", "attributes": { "href": "https://cex.io/r/0/up105394792/0/", "target": "_blank", "style": { "float": "right" } }, "children": [{ "type": "Element", "tagName": "img", "attributes": { "src": "https://cex.io/rb/CEX-3-160x600.png", "alt": "CEX.IO Bitcoin Exchange", "width": 160, "height": 600, "border": 0 }, "children": [] }] }],
+
                 },
                 analytics: {
-                    google: "UA-106566819-1",
+                    google: "UA-106566819-2"
                 },
                 exchanges: [{
-                    name: 'novaexchange.com',
-                    url: 'https://novaexchange.com/market/BTC_ONION/?re=pud127q1c1b3989waadm',
-                    avatar: 'https://pbs.twimg.com/profile_images/756955381658685440/qAk_ejcm_200x200.jpg',
+                    name: 'cryptopia',
+                    url: 'https://www.cryptopia.co.nz/Exchange/?market=LTC_BTC&referrer=cryptpiaff',
+                    avatar: 'http://i.imgur.com/nSUbpiq.png',
+                    description: 'Established exchange with support for many altcoins and unique features such as marketplace for goods'
+                }, {
+                    name: 'c-cex.com',
+                    url: 'https://c-cex.com/?p=ltc-btc&rf=FECEAFAABCEB70DC',
+                    avatar: 'https://www.cryptocompare.com/media/20008/ccex-logo.png?width=200',
                     description: 'Easy to use, anonymous friendly, wide varaiety of altcoins to trade'
+                }, {
+                    name: 'cex.io',
+                    url: 'https://cex.io/r/0/up105394792/0',
+                    avatar: 'https://lh3.googleusercontent.com/pPihmJ3lDGIpgegOSFkN1r5HJxaxPgTMoD0jlkxXEFoxy1FjRGp4gMsmw0hnYn_xVBo=w100',
+                    description: 'Easy to use, secure, cashout to Fiat currency'
                 }]
             }
             iConfig.save().then(cb(null, 'init config success'))
         }
     })
 }
- 
